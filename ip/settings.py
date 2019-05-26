a31bccfc2eb9e80bb7340a3208f6983e5f2dada4
Elle <mitchellemakini15@gmail.com>
2019-05-27 00:04:53 +0300

    added database setting to pyschopg2

--- ip/settings.py
+++ ip/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'ip.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'review',
         'USER': 'user',
         'PASSWORD':'p',
