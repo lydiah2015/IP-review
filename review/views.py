7f78541100e28fa381231d7da77f1794e0e698fd
Elle <mitchellemakini15@gmail.com>
2019-05-26 21:20:05 +0300

    created a login view

--- review/views.py
+++ review/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .models import Project,Profile,Comment
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def posts(request):
-    return render(request, 'all-posts/posts.html')
+    project = Project.objects.all()
+    return render(request, 'all-posts/posts.html',{"projects":project})
