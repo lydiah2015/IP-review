c9bb278d08abf54d083a9f7365d98ebc15c345a7
Elle <mitchellemakini15@gmail.com>
2019-05-27 00:06:21 +0300

    added a url to direct to my profile

--- review/urls.py
+++ review/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns=[
      url('^$',views.posts,name='posts'),
-    
+     url(r"^accounts/profile/$", views.my_profile, name = "my_profile"),
+     # url(r"^profile/update/$", views.update_profile, name = "update_profile"),
+     
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
