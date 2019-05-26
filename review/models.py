67b0f1cf09f25dca2938079869333d2299e3ffd9
Elle <mitchellemakini15@gmail.com>
2019-05-26 21:22:24 +0300

    added three models that map to the database

--- review/models.py
+++ review/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 # Create your models here.
 class Profile(models.Model):
     profile_photo = models.ImageField(upload_to='image/')
     biography = models.CharField(max_length=50)
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='profile')
+        User, on_delete=models.CASCADE)
 
+class Comment(models.Model):
+    comm = models.TextField(max_length=100)
+    user = models.ForeignKey(User,
+                             blank=True, on_delete=models.CASCADE)
 
-class Projects(models.Model):
-    user = models.ForeignKey(User, related_name="posts", blank=True)
-    image = ImageField(blank=True, manual_crop="")
+class Project(models.Model):
+    user = models.ForeignKey(User, blank=True)
+    image = models.ImageField(upload_to='image/')
     description = models.CharField(max_length=50)
-    comments = models.CharField(max_length=50)
+    comm = models.ForeignKey(Comment)
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField()
+
+
