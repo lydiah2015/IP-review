eb3cceb218fa34d7a1b7fe2cd0ac56c333864d41
Elle <mitchellemakini15@gmail.com>
2019-05-27 16:36:58 +0300

    added all initial changes

--- review/urls.py
+++ review/urls.py
@@ -3,11 +3,14 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from . import views
 
-urlpatterns=[
-     url('^$',views.posts,name='posts'),
-     url(r"^accounts/profile/$", views.my_profile, name = "my_profile"),
-     # url(r"^profile/update/$", views.update_profile, name = "update_profile"),
-     
+urlpatterns = [
+    url('^$', views.posts, name='posts'),
+    url(r"^accounts/profile/$", views.my_profile, name="my_profile"),
+    url(r"^comment/$", views.comment, name="comment"),
+    url('^new_project', views.new_project, name='new_project'),
+    url(r"^profile/update/$", views.update_profile, name="update_profile"),
+
 ]
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
