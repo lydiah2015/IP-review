5dc1eb64e7fa7003994bfa28b487fb631dcb397d
Elle <mitchellemakini15@gmail.com>
2019-05-27 16:32:52 +0300

    added ProfileForms as well as ProjectForm

--- review/forms.py
+++ review/forms.py
@@ -1,7 +1,36 @@
-# from django import forms
-# from .models import Profile
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import Profile, Project, Comment
 
 
-# class ProfileForm(forms.Form):
-#     biography = forms.CharField(label="Biography")
-#     profile_photos = forms.ImageField(upload_to='image/')
+class ProjectForm(forms.ModelForm):
+    '''
+    New project creation form.
+    '''
+
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Project Title'}))
+    description = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Description'}))
+    url = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Project Link'}))
+    rating = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Project rating'}))
+
+    class Meta:
+        model = Project
+        fields = ['title', 'image', 'description', 'url', 'rating']
+
+
+class ProfileForm(forms.ModelForm):
+    '''
+    New project creation form.
+    '''
+
+    bio = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Input Bio, something catchy'}))
+
+    class Meta:
+        model = Profile
+        fields = ['bio', 'profile_photo']
